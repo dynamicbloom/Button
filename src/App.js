f7432c2c178911b8eafcbe7f72ad2403188dccc1
Geo Nuikin <georgychannel@gmail.com>
2020-12-11 14:14:21 +0300

    More stylized default button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 10px 60px;
+  border-radius: 5px;
+  font-size: 20px;
+  outline: 0;
+  box-shadow: 0px 2px 2px lightgray;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
 
 function clickMe() {
     alert('Hello World!')
-
 }
 
 export default function App() {
     return (
         <div>
-            <button onClick={clickMe}>
+            <Button onClick={clickMe}>
                 Click
-            </button>
+            </Button>
         </div>
-
     );
-
 }
