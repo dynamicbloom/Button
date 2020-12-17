3a2f6eed546f9eeb3934ddaf248dffb1ceca748b
Geo Nuikin <georgychannel@gmail.com>
2020-12-17 18:51:34 +0300

    darkbrown button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button`
-  background-color: #855800;
+  background-color: #66480c;
   padding: 10px 60px;
   border-radius: 5px;
   font-size: 20px;
