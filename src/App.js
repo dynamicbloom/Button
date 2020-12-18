a974534a5273606a7769b4b8041ede7bb6e6408e
Geo Nuikin <georgychannel@gmail.com>
2020-12-18 15:09:11 +0300

    Hover and theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const theme = {
+    red: {
+        default : '#d42f2f',
+        hover: '#ba2b2b'
+    }
+}
+
 const Button = styled.button`
-  background-color: #d32f2f;
+  background-color: ${props => theme[props.theme].default};
+  color: white;
   padding: 10px 60px;
   border-radius: 5px;
   font-size: 20px;
@@ -10,7 +18,15 @@ const Button = styled.button`
   box-shadow: 0px 2px 2px lightgray;
   text-transform: uppercase;
   cursor: pointer;
+  transition: ease background-color 250ms;
+  &: hover {
+    background-color: ${props => theme[props.theme].hover};
+  }
 `;
+
+Button.defaultProps = {
+    theme: 'red'
+}
 
 function clickMe() {
     alert('Hello World!')
